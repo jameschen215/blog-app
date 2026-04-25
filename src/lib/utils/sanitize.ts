93b57dd3fc74b25faa3904d86669761b33ec8b67
James <chenjames215@gmail.com>
2026-04-25 18:25:38 +0800

    fix: improve santizeHtml fucntion to include more tags and attributes

--- src/lib/utils/sanitize.ts
+++ src/lib/utils/sanitize.ts
@@ -1,8 +1,42 @@
-// $lib/utils/sanitize.ts
+// // $lib/utils/sanitize.ts
+// import DOMPurify from 'isomorphic-dompurify';
+
+// export function sanitizeHtml(html: string): string {
+// 	// If content has no HTML tags, wrap paragraphs
+// 	if (!/<[a-z][\s\S]*>/i.test(html)) {
+// 		html = html
+// 			.split(/\n/)
+// 			.map((p) => `<p>${p}</p>`)
+// 			.join('');
+// 	}
+
+// 	return DOMPurify.sanitize(html, {
+// 		ALLOWED_TAGS: [
+// 			'p',
+// 			'br',
+// 			'strong',
+// 			'em',
+// 			'u',
+// 			'a',
+// 			'ul',
+// 			'ol',
+// 			'li',
+// 			'h1',
+// 			'h2',
+// 			'h3',
+// 			'blockquote',
+// 			'pre',
+// 			'code',
+// 			'hr'
+// 		],
+// 		ALLOWED_ATTR: ['href', 'target', 'rel']
+// 	});
+// }
+
 import DOMPurify from 'isomorphic-dompurify';
 
 export function sanitizeHtml(html: string): string {
-	// If content has no HTML tags, wrap paragraphs
+	// Only wrap in paragraphs if content has no HTML tags at all
 	if (!/<[a-z][\s\S]*>/i.test(html)) {
 		html = html
 			.split(/\n/)
@@ -17,6 +51,7 @@ export function sanitizeHtml(html: string): string {
 			'strong',
 			'em',
 			'u',
+			's',
 			'a',
 			'ul',
 			'ol',
@@ -24,11 +59,13 @@ export function sanitizeHtml(html: string): string {
 			'h1',
 			'h2',
 			'h3',
+			'h4',
 			'blockquote',
 			'pre',
 			'code',
-			'hr'
+			'hr',
+			'img'
 		],
-		ALLOWED_ATTR: ['href', 'target', 'rel']
+		ALLOWED_ATTR: ['href', 'target', 'rel', 'class', 'src', 'alt']
 	});
 }
